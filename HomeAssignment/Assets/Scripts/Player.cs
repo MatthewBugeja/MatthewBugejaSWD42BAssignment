0ae240b082300b1f7717f06b8ed0a670dba11ac9
MatthewBugeja <73161880+MatthewBugeja@users.noreply.github.com>
2020-12-29 15:15:11 +0100

    Working On Home Assignment
    Things that have been done:
    
    1. Created a Scripts folder inside the Assets folder;
    2. Created a Player script inside the Scripts folder;
    3. Added the Player script as a component to the PlayerCar;
    4. Made the PlayerCar move left and right;
    5. Used Time.deltaTime to make the game frame rate independent;
    6. Created a SerializedField to be able to adjust the PlayerCar movement speed;
    7. Created another method inside the Playe script;
    8. This method created a boundary for the PlayerCar, this was done by using ViewPortToWorldPoint(). This means that the PlayerCar will not go out of the camera view;
    9. Created a padding so that the car can always be seen as a whole.

---
+++ HomeAssignment/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 2f;
+
+    float xMin, xMax, yMin, yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //method to be able to move the PlayerCar left and right
+    private void Move()
+    {
+        //delatX will be updated with the input that will happen on the x-axis, left and right
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //newXPos   = current x-position   + difference in X
+        var newXPos = transform.position.x + deltaX;
+
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        //the x-position will be updated according to the newXPos - whether left or right
+        //Y position will remain the same
+        transform.position = new Vector2(newXPos, transform.position.y);
+
+    }
+
+    private void SetBoundaries()
+    {
+        //get the main camera from Unity
+        Camera gameCamera = Camera.main;
+
+        //set the boundaries on the x-axis
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        //set the boundaries on the y-axis
+        //NOT needed but done just in case of a bug where the PlayerCar could move up and down aswell
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
