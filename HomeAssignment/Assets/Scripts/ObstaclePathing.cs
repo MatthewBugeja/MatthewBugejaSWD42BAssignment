6e9ccd2998f8825d4dd3f32ff4825aa93ab0057e
MatthewBugeja <73161880+MatthewBugeja@users.noreply.github.com>
2020-12-31 12:36:51 +0100

    Working On Home Assignment
    Things that have been done:
    
    1. Created a Prefabs folder inside the Asstes folder;
    2. Dragged all suitable asstes and GameObjects apart from the EventSystem into the Prefabs folder;
    3. Created an empty GameObject 'Shredder' and placed it and the bottom of the camera and edited the width to cover the whole width of the camera;
    4. Created a Shredder script inside the Scripts folder;
    5. Added the Shredder script as a component to the Shredder GameObject;
    6. The Shredder script was coded to destroy any GameObjects that collide with Shredder GameObject;
    7. Created an empty GameObject 'Path (0)';
    8. Created an empty GameObject 'Waypoint (0)';
    9. Dragged 'Waypoint (0)' into 'Path (0)';
    10. Duplicated 'Waypoint (0)' multiple times and positioned them how I wanted;
    11. This created a path with multiple waypoints for the first obstacle to follow;
    12. Created an ObstaclePathing script inside the Scripts folder;
    13. Added a SerializedField of type List that stores Transform information named 'waypoints';
    14. Created a Paths folder and dragged the waypoints then the path into the Paths folder;
    15. Opened the 1st obstacle and dragged the waypoints into the ObtaclePathing list;
    16. Added a method called 'ObstacleMove()' inside the ObstaclePathing script;
    17. Coded the above method to move the 1st obstacle on the 1st path.

---
+++ HomeAssignment/Assets/Scripts/ObstaclePathing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePathing : MonoBehaviour
+{
+    //a list of type Transform
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 5f;
+    
+    int waypointIndex = 0; //shows in which waypoint the obstacle is in
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the starting position for the obstacle to the 1st waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ObstacleMove();
+    }
+
+    void ObstacleMove()
+    {
+        if (waypointIndex <= waypoints.Count -1)
+        {
+            //targetPosition - where the obstacle needs to move to next
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            //make sure that the z position is 0
+            targetPosition.z = 0f;
+
+            //set the movement speed per frame
+            var obstacleMovement = moveSpeed * Time.deltaTime;
+
+            //move the obstacle from the current position, to the target position, at the movement speed per frame
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, obstacleMovement);
+
+            //if targetPosition is reached
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        //if the enemy reaches the last waypoint
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
