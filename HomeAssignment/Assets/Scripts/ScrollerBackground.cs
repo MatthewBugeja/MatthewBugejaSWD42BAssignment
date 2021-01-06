1dcd04142c083cc5dc721e1736d7aae14c5ef262
MatthewBugeja <73161880+MatthewBugeja@users.noreply.github.com>
2021-01-06 15:22:54 +0100

    Working On Home Assignment - Task 1 Finished
    Things that have been done:
    
    1. Created a Quad in the hierarchy, named 'Background', and the 'Mesh Collider' was removed from the Inspector;
    2. The Background image from the hierarchy and from the Prefabs folder was deleted;
    3. Selected the Background image from the Sprites folder;
    4. Set the 'Texture Type' to 'Default' and the 'Wrap Mode' to 'Repeat', then applied the changes;
    5. Dragged the Bachground image from the Sprites folder onto the Quad Background in the hierarchy;
    6. In the inspector from the Background image in the hierarchy, from the 'Shader' dropdown the 'Unlit' option was chosen then the 'Texture' option was chosen;
    7. The Background image was dragged into the Prefabs folder;
    8. Created a 'ScrollerBackground' script inside the Scripts folder;
    9. Added the appropriate variables inside the script;
    10. The script was then coded to scroll the background image at an appropriate speed.

---
+++ HomeAssignment/Assets/Scripts/ScrollerBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollerBackground : MonoBehaviour
+{
+    //the scrolling speed
+    [SerializeField] float backgroundScrollSpeed = 5f;
+    //the material from the texture
+    Material myMaterial;
+    //the movement
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the material of the background from the Renderer component
+        myMaterial = GetComponent<Renderer>().material;
+        //will scroll in the y-axis at the followingg speed
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the material mainTextureOffset by offSet every frame
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
